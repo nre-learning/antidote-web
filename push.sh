2ac07c3dd22375d4b2a07b18c99bf34e211f6ad1
Hayk Galstyan <haykokoryun@gmail.com>
2018-10-08 01:00:47 +0400

    update push.sh to set COMMIT_SHA build arg

--- push.sh
+++ push.sh
@@ -1,2 +1,4 @@
-docker build -t antidotelabs/antidote-web -f Dockerfile .
+COMMIT_SHA=$(git rev-parse HEAD)
+
+docker build --build-arg COMMIT_SHA=$COMMIT_SHA -t antidotelabs/antidote-web -f Dockerfile .
 docker push antidotelabs/antidote-web:latest
