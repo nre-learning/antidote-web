d102463b530816778163f2d36c1896b76de6c600
Nils Lundquist <nils@bitovi.com>
2020-01-09 09:26:48 -0700

    move components to antidote-ui-components repo. this repo will now just host the page structure and application bundling. added. show lab guide in mobile via tab from light-DOM use --vv-height var to compensate page height for iOS keyboards etc. some mobile styling.

--- src/main/webapp/rollup.config.js
+++ src/main/webapp/rollup.config.js
@@ -4,6 +4,7 @@ import alias from '@rollup/plugin-alias';
 import commonjs from 'rollup-plugin-commonjs';
 import { terser } from 'rollup-plugin-terser';
 
+// if this is not a "watched" build, assume it's a prod build
 const production = !process.env.ROLLUP_WATCH;
 
 export default {
@@ -14,18 +15,19 @@ export default {
         sourcemap: true
     },
     plugins: [
-        // use to sub-in un-minified resources during debugging (some modules have been minified by default)
+        // use to sub-in un-minified resources during debugging (for those modules that are shipped minified by default)
         // alias({
         //     entries: [
         //         { find: 'xterm-addon-fit', replacement: path.resolve(__dirname, 'node_modules/xterm-addon-fit/out/FitAddon.js') },
         //     ]
         // }),
-        resolve(), // tells Rollup how to find date-fns in node_modules
+        resolve(),
         commonjs({
             namedExports: {
+                // needed for xterm compatibility w/ rollup
                 'xterm': [ 'Terminal' ]
             },
-        }), // converts date-fns to ES modules
+        }),
         production && terser() // minify, but only in production
     ]
 };
