bcdfc915889aff09ae600fce119698a08d6b10c4
Nils Lundquist <nils@bitovi.com>
2020-01-13 11:54:59 -0700

    move components to antidote-ui-components repo. this repo will now just host the page structure and application bundling. get resource bundles from node dependencies

---
+++ src/main/webapp/antidote-config.js
@@ -0,0 +1,9 @@
+export default {
+  // To specify the text to be used by this deployment of Antidote, define either:
+  //   locale - a country code with a associated localization in the `antidote-localizations` modules
+  //     e.g: "locale": "en"
+  //   localization-module - a NPM module name that exports a localization object
+  //     e.g: "localization-module": "nre-antidote-localization",
+
+  "locale": "en"
+}
--- src/main/webapp/js/antidote.js
+++ src/main/webapp/js/antidote.js
@@ -1,10 +1,17 @@
 // Notes on Bundling:
 // This module imports all the dependencies used by the Antidote web application. After running the
-// bundler this results in a monolithic file containing all JS dependencies needed for the app.
+// bundler this results in a monolithic file containing the JS dependencies needed for all pages of
+// the app.
 //
 // Keeping this monolithic was done for simplicities sake.
 // Individual bundles could be made for each page, but that is a unnecessary optimization given the
 // rather small size of the monolithic bundle.
+
+// import antidote resources bundles (text & styles) and set them in their global locations rather
+// than configuring per-component or using component defaults
+import l8n from 'antidote-localization' // this module is aliased to the desired localization in rollup.config.js
+window.antidoteLocalization = l8n;
+window.antidoteStyleSheet = '/node_modules/nre-styles/dist/styles.css';
 
 // required for correct lab page height on iOS
 import 'antidote-ui-components/helpers/visual-viewport-height';
